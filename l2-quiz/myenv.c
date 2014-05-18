5c1aa97743bd235bdb61aaff45dbfa775bec5aaf
wikibootup <wikibootup@gmail.com>
2014-05-18 18:39:54 +0900

    basic_Unix

--- l2-quiz/myenv.c
+++ l2-quiz/myenv.c
@@ -1,30 +1,39 @@
+//for fgets(3), fputs(3)
 #include <stdio.h>
 //for extern char **environ
 #include <stdlib.h>
-//for getpid()
+//for getpid(), read(2)
 #include <unistd.h>
+//for open(2)
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 //for strcpy()
 #include <string.h>
+//for exit()
+#include <stdlib.h>
 
-extern char **environ;
+//constant BUFSIZ is defined in stdio.h
+
+//extern char **environ;
 
 int main(int argc, char *argv[])
 {	
 	char buf[BUFSIZ];
 	char result[BUFSIZ];
-	char **env;
+//	char **env;
+//	char thisPID[20];
 
-	int n;
-	extern char *optarg;
-	extern int optind;
-	
+	int fd, n;
+	int i = 0;
+
+//	FILE *rfp;
+//	char result[BUFSIZ];
+//	char buf[BUFSIZ];
+
+
 	if(argc == 1){
 			sprintf(buf, "%d", (int)getpid() );
-//			env = environ;
-//			while(*env) {
-//				printf("%s\n", *env);
-//				env++;
-//			}
 	}
 	else if(argc == 2)
 		strcpy(buf, argv[1]);
@@ -33,10 +42,51 @@ int main(int argc, char *argv[])
 	}
 
 //sprintf(result, "ADD1 %s ADD2", buf);
-	sprintf(result, "/proc/%s/environ", buf);	
-	printf("RESULT : %s\n", result);
+	sprintf(result, "/proc/%s/environ", buf);
+//	printf("%s\n", result);	
+/*
+	if( (rfp = fopen(result, "r")) == NULL) {
+		perror("fopen : unix.txt");
+		exit(1);
+	}
+	
+	while(fgets(buf, BUFSIZ, rfp) != NULL) {
+		fputs(buf, rfp);
+	}
+	
+	fclose(rfp);
 
+*/
+	fd = open(result, O_RDONLY);	
+	if(fd == -1) {
+		perror("Open");
+		exit(1);
+	}
+
+	n = read(fd, buf, BUFSIZ);
+	if(n == -1) {
+		perror("Read");
+		exit(1);
+	}
+//	printf("%d", n);
+	buf[n] = '\0';		
+//	printf("%s\n\n\n", buf);
+
+	while(i < n) {
+		if(*(buf + i) != '\n') {
+			printf("%c", *(buf + i++));
+		}
+		else {
+			printf("%c", *(buf + i++));
+			printf("\n");
+		}
+	}	
+	printf("\n");
+	
+//	printf("RESULT : %s\n", result);
+	close(fd);
 
 	return 0;
 }
+
 
